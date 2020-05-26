3203d005106913e7a2a61462944b02a989efa480
Jun Ming <ojmjunming@gmail.com>
2020-05-27 02:30:17 +0800

    init commit for client side, added react-router-dom, eslint

---
+++ battleships_client/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import { Game, Home } from './pages';
+import './index.css';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/game' component={Game} />
+                <Route render={() => <h1>404 You are not supposed to see this! 404</h1>} />
+            </Switch>
+        </Router>
+    );
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <App />
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
---
+++ battleships_client/src/pages/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Game = () => {
+    return (
+        <div>
+            Game PAGE
+        </div>
+    );
+};
+
+export default Game;
