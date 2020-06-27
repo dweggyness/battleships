40e8e8dbeb92ae81dfb3206066fa941128940659
dweggyness <ojmjunming@gmail.com>
2020-06-28 03:10:29 +0800

    Revert "added route handling for heroku"
    This reverts commit 8ba705af4976f0757dd73caf120cf8e84168bad3.

--- battleships_server/index.js
+++ battleships_server/index.js
@@ -2,11 +2,9 @@
 const express = require('express');
 
 const app = express();
-const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
-
 exports.io = io;
 const { setRoutes } = require('./src/routes');
 
@@ -17,12 +15,6 @@ async function startServer() {
     app.use(bodyParser.urlencoded({ extended: false }));
 
     await setRoutes(app);
-
-    app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-    app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-    });
 
     http.listen(port, () => console.log(`Server started at port ${port}`));
 }
